64b0129e432c7e74869f966d751805b6147e20ff
Vinicius Sossei Sakugawa <viny.sossei@icloud.com>
2020-07-13 17:52:40 -0300

    Adicionado SplashScreen e implementado verificação de primeiro acesso

--- app/src/main/java/br/com/igti/projetohelloworld/SplashScreenActivity.java
+++ app/src/main/java/br/com/igti/projetohelloworld/SplashScreenActivity.java
@@ -2,7 +2,11 @@ package br.com.igti.projetohelloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        checkOpen();
+    }
+    private void checkOpen(){
+        SharedPreferences pref= getSharedPreferences("preferences_projeto_hellowrold_igti", Context.MODE_PRIVATE);
+        boolean isFirstTime = pref.getBoolean("key_firsttime",true);
+        if(isFirstTime){
+            openDelayed();
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putBoolean("key_firsttime",false);
+        }else{
+            openActvity();
+        }
+    }
+    private void openDelayed(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                openActvity();
+            }
+        }, 4000);
+    }
+
+    private void openActvity() {
+        startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
     }
 }
