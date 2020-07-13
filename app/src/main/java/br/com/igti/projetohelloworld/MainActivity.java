43d2f833bb9cfcc98150223fed88503cf509e39f
Vinicius Sossei Sakugawa <viny.sossei@icloud.com>
2020-07-13 17:33:41 -0300

    Sobrescreva os métodos onStart(), onRestart(), onResume(), onPause(), onStop() e onDestroy(), na classe MainActivity.    Imprima no LogCat a sequência do ciclo de vida.

--- app/src/main/java/br/com/igti/projetohelloworld/MainActivity.java
+++ app/src/main/java/br/com/igti/projetohelloworld/MainActivity.java
@@ -3,12 +3,50 @@ package br.com.igti.projetohelloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate()");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop()");
     }
 }
