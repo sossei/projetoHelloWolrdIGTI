ce6712fbe5904db1e217593b270284bceadc6642
Vinicius Sossei Sakugawa <viny.sossei@icloud.com>
2020-07-13 18:43:07 -0300

    Criação dos flavors escuro e claro

--- app/src/main/java/br/com/igti/projetohelloworld/Tela2Activity.java
+++ app/src/main/java/br/com/igti/projetohelloworld/Tela2Activity.java
@@ -1,23 +1,56 @@
 package br.com.igti.projetohelloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Tela2Activity extends AppCompatActivity {
 
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(Objects.equals(intent.getAction(), Intent.ACTION_BATTERY_CHANGED)){
+                Toast.makeText(getApplicationContext(), "Atenção!!! Bateria Fraca", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
         configurandoListView();
+
+    }
+
+    private void configurandoBoradcastReceiver() {
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        this.registerReceiver(broadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        configurandoBoradcastReceiver();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
     }
 
     private void configurandoListView() {
